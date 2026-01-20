2ef8e59d7437aa69dfb8e70822aa425d458f3130
Muhammad Owais Javed <k-mjaved@c2r2p2.hive.fi>
2026-01-20 13:40:05 +0200

    Changes

--- casting.go
+++ casting.go
@@ -1,5 +1,5 @@
 package sprint
-
+import "math"
 func Casting(n float64) int {
   var x int = int(math.Round(n))
   return x
--- hello/main.go
+++ hello/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    
+ a=Casting(3.456)
+ fmt.Println("Hello, World!")
+ fmt.Print(a)
+}
+
+func Casting(n float64) int {
+  var x int = int(math.Round(n))
+  return x
+
 }
 
 
