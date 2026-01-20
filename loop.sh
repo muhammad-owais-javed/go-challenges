d4dbbf74a536ec319a22fde74c4c6a052322b77f
Muhammad Owais Javed <k-mjaved@c2r2p2.hive.fi>
2026-01-20 10:26:30 +0200

    Changes

--- loop.sh
+++ loop.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-	exit 1
-fi
- 
-if [[ "$1" = *[!0-9]* ]]; then
-	exit 1
+n=$1
+
+if (( $n >100 )); then
+	n=100
 fi
 
-value=$1
-
-if [[ $value -gt 100 ]]; then
-	
-	for i in {1..100}
-	do
-		echo "This is loop number $i"
-	done
-
-fi
-
+for i in $(seq 1 $n);
+do 
+ printf "This is loop number $i\n"
+done
