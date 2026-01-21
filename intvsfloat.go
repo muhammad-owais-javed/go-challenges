9d30d4042376e8192f1ec1b901d7d26ea30788b1
Muhammad Owais Javed <k-mjaved@c2r1p6.hive.fi>
2026-01-21 12:13:14 +0200

    Changes

--- intvsfloat.go
+++ intvsfloat.go
@@ -2,7 +2,9 @@ package sprint
 
 func IntVsFloat(i int, f float32) string {
 
- if ( float32(i) > f ){
+if ( float32(i) == f ){
+	return "Same"
+} else if ( float32(i) > f ){
 	return "Integer"
 } else {
 	return "Float"
