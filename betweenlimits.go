cb6b4d74d11f6dd07185f742f7b01784e8755d5d
Muhammad Owais Javed <k-mjaved@c2r1p6.hive.fi>
2026-01-21 17:05:06 +0200

    Changes

--- betweenlimits.go
+++ betweenlimits.go
@@ -2,6 +2,10 @@ package sprint
 
 func BetweenLimits(from, to rune) string {
 
+ if from > to {
+	from, to = to, from
+ }
+
  var resultrune []rune
  for i:=from+1 ; i<to ; i++ {
  	resultrune := append(resultrune, i)
