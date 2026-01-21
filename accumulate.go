66f827d75cf7a6e0e2a039a46a271b8bdcea168a
Muhammad Owais Javed <k-mjaved@c2r1p6.hive.fi>
2026-01-21 12:23:40 +0200

    Changes

---
+++ accumulate.go
@@ -0,0 +1,17 @@
+package sprint
+
+func Accumulate(n int) int {
+
+var result int = 0
+
+ if ( n == 0 ){
+	return result
+ } else if ( n > 0 ) {
+  
+ for ( var i int = 0; i<=4; i++)
+ 	result + = i
+ }
+ return result
+
+}
+
