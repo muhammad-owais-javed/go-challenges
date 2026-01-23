c6e1aaf0cc884abd73bc057213d60a1db4a90607
Muhammad Owais Javed <k-mjaved@c2r4p6.hive.fi>
2026-01-23 16:27:22 +0200

    Changes

---
+++ alphabemastery.go
@@ -0,0 +1,17 @@
+package sprint
+
+func Countdown(n int) string {
+
+	var result string
+
+	for i:=n ; i > 0; i-=2 {
+
+	numberAsChar = '0' + i
+	result += string(numberAsChar)
+	result += ", "
+	}
+
+	result = result[:len(result)-2]
+	return result + ", 0!"
+
+}
--- hello/main.go
+++ hello/main.go
@@ -4,15 +4,13 @@ import "fmt"
 
 func main() {
 
- fmt.Println("Hello, World!")
- var status bool 
- if (1 > 0 ) {
-    
-    status = true
-    fmt.Println(status)
+   fmt.Println("Hello, World!")
  
- }
- 
+}
+
+func AlphabetMastery(n int) string {
+
+
 }
 
 
