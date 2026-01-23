a4be8786a5637c95bfee9ee511323bf2d94e6b5d
Muhammad Owais Javed <k-mjaved@c2r4p6.hive.fi>
2026-01-23 19:44:15 +0200

    Changes

---
+++ pairs.go
@@ -0,0 +1,18 @@
+package main
+
+//import "fmt"
+
+func Pairs() string {
+
+   result := ""
+    for i := 0; i<100; i++ {
+	for j := i; j<100; j++ {
+	   if i != j {
+	     result += fmt.Sprintf("%02d %02d, ", i, j)
+		}
+	}
+  }
+  result = result[:len(result)-2]
+//  fmt.Println("Result:", string(result))
+  return result
+}
