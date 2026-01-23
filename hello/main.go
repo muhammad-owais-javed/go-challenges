88073ed027370746cc354f3b23fcc8cfe9a2b3d9
Muhammad Owais Javed <k-mjaved@c2r4p6.hive.fi>
2026-01-23 18:01:14 +0200

    Changes

--- hello/main.go
+++ hello/main.go
@@ -5,12 +5,29 @@ import "fmt"
 func main() {
 
    fmt.Println("Hello, World!")
- 
+   
+   fmt.Println(string(Countdown(7)))
+  
 }
 
-func AlphabetMastery(n int) string {
+func Countdown(n int) string {
 
+   result := ""
+   
+   for i := n; i < n; i-=2 {
 
+      result = result + string('0' + rune(i)) + ", "
+      //result += string(rune(int(i)) + 'a') + "," + " "
+      // result += string(i)
+      //fmt.Println("Inside for block")
+      //fmt.Println("LoopNumber:", i)
+      // return result
+    }
+    // return "0!"
+   return result
 }
+
+
+
 
 
