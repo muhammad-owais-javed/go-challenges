513a2f1f9cda018de7e4e74daed2069346541589
Muhammad Owais Javed <k-mjaved@c2r4p6.hive.fi>
2026-01-23 18:05:51 +0200

    Changes

--- alphabetmastery.go
+++ alphabetmastery.go
@@ -3,17 +3,12 @@ package sprint
 import "fmt"
 
 func AlphabetMastery(n int) string {
-
-	const alphabet = "abcdefghijklmnopqrstuvwxyz"
-	var result string
-
-	if n > 0 && n < 26 {
-		for  i := 0; i < n  ; i++ {
-			
-			result += string(alphabet[i])
-		}
+	
+	result := ""
+	
+	for i := 0 ; i < n; i++ {
+		result += string('a' + rune(i))
 	}
-
+	
 	return result
-
 }
