b3503e427da6bc2397bf88b0ff191c3043f7e0a8
Muhammad Owais Javed <k-mjaved@c2r3p2.hive.fi>
2026-01-26 20:02:59 +0200

    Changes

---
+++ filterbysum.go
@@ -0,0 +1,17 @@
+package main
+
+func FilterBySum(arr [][]int, limit int) [][]int {
+	arr1 := make([][]int, 0)
+
+	for i := 0 ; i < len(arr); i++ {
+		sum := 0
+		for j:=0 ; j < i ; j++ {
+			sum += j
+		}
+		if sum >= limit {
+			arr1 = append(arr1, i)
+
+		}
+	}
+	return arr1
+}
