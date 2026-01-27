116d0746c988539a75288bbcc76556d856c0bfc2
Muhammad Owais Javed <k-mjaved@c2r4p4.hive.fi>
2026-01-27 21:00:55 +0200

    Changes

--- isnumeric.go
+++ isnumeric.go
@@ -1,14 +1,16 @@
-package sprint
+func IsNumeric(s string) bool  {	
+  
+  if s == "" {		
+    return false	
+    }	
 
-func IsNumeric(s string) bool {
+    for _, r := range s {		
+      
+      if r < '0' || r > '9' {
+        	return false		
+      }
 
-  var check bool
-  for _, r := range s {
-
-	if  r < "0" || r > "9" {
-	return false 
-	}
-  }
-
+  }	
+    
   return true
 }
