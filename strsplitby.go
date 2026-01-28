98164e2ad196d9c586059103106c1578eaae23c8
Muhammad Owais Javed <k-mjaved@c2r1p4.hive.fi>
2026-01-28 10:48:15 +0200

    Testing

--- strsplitby.go
+++ strsplitby.go
@@ -4,7 +4,7 @@ func StrSplitBy(s, sep string) []string {
 	var result []string
 	word := ""
 
-	for i := 0; i < len(s); {
+	for i := 0; i < len(s)-1; {
 		
 		if s[i:i+len(sep)] == sep {
 			result = append(result, word)
