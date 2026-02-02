552bb44d1d30fae84c5b4d106fd661c224d66531
Owais <k-mjaved@c2r5p2.hive.fi>
2026-02-02 16:52:03 +0200

    Changes

--- factorialrecursive.go
+++ factorialrecursive.go
@@ -1,6 +1,6 @@
 package sprint
 
-func FactorialIterative(n int) int {
+func FactorialRecursive(n int) int {
 
  if n <= 1 && n >= 0  {
   return 1
@@ -9,6 +9,6 @@ func FactorialIterative(n int) int {
  if n < 0 {
   return 0
 }
- return n * FactorialIterative(n)
+ return n * FactorialRecursive(n)
 
 }
